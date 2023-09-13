9cddcf96bee2d90d1a33855b3ff2def475ea2636
Ryuichi.Ishitsuka <ryuichi.ishitsuka@gmail.com>
2023-09-13 18:56:55 +0900

    Fix release

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,6 @@ publishing {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/ryu1/amazon-pay-api-sdk-java-wrapper")
-            println(System.getenv("GITHUB_PACKAGE_USER_NAME"))
-            println(System.getenv("GITHUB_PACKAGE_TOKEN"))
             credentials {
                 username = System.getenv("GITHUB_PACKAGE_USER_NAME")
                 password = System.getenv("GITHUB_PACKAGE_TOKEN")
